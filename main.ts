d2aae2cf53b44c769b53da8d6aa5e19acb1ad1d0
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-07 17:07:05 +0200

    prevent splitting footnotes & pandoc citations

--- main.ts
+++ main.ts
@@ -15,6 +15,10 @@ export default class ObsidianSemBr extends Plugin {
 
 	async onunload() { console.log("Semantic Line Breaker Plugin unloaded.") }
 
+	public removeSemBr (str: string) {
+		return str.replace (/([.,:;?!—]) ?\n(?!\n)/gm, "$1 ");
+	}
+
 	async toggleSemBr (editor: Editor) {
 		// read note
 		let noteContent = editor.getValue()
@@ -46,38 +50,51 @@ export default class ObsidianSemBr extends Plugin {
 		const isSemanticLineBreaked = /[.,:;?!—] ?\n(?!\n)/.test(noteContent);
 
 		proseTextArr = proseTextArr.map(prose => {
-			if (isSemanticLineBreaked) return prose.replace (/([.,:;?!—]) ?\n(?!\n)/gm, "$1 ");
+			// Remove sembr
+			if (isSemanticLineBreaked) return this.removeSemBr(prose);
 
-			return prose.replace (
-				/([^.,|]{15,}?[^\]:1-9][.,:;?!—](?: ?\[.+\])? )(?!\n\n| |.*\|.*$)/gm,
-				//(1         )(2      )(3      )(4         )(5)(6               )
-				//(     $1                                    )
-				// 1: 15 chars minimum (non-greedy), all without another , or .  occuring
+			// Add sembr
+			prose = prose.replace (
+				/([^|.]{25,}?[^:][.,:;?!—](?: ?\[.+\])?( ))(?!\n\n| |.*\|.*$|p\. [1-9-]+\]|@|\d)(?=[^|.]{25,})/gm,
+				//(1         )(2 )(3      )(4          )(5) (6                )(7              )(8           )
+				//(     $1                                 )
+				// 1: 20 chars minimum (non-greedy), all without another . occuring
 				//    pipes (|), too, aren't allowed (tables)
-				// 2: not ending with ]: (footnotes), :: (dataview inline fields), or
-				//    digit (enumration or floating number)
-				// 3: ending with a punctuation as defined in SemBr specification §4 and §5
+				// 2: not ending with :: (dataview inline fields)
+				// 3: ending with a punctuation as defined in SemBr specification §4-5
 				// 4: optionally followed by a footnote like `[^1]`
 				// 5: space after punctuation in prose text
 				// 6: not followed by two line breaks (blank line),
 				//    another space (Markdown Two-Space Rule),
 				//    or a pipe character somewhere till the end of the line (Table)
+				// 7: also not followed by pandoc citation syntax, so citations aren't
+				//    split up
+				// 8: followed by at least 20 chars without punctuation
+				//    (to prevent too short parts afterwards)
 
 				"$1\n"); // add the line break
+
+			// stitch back footnotes
+			prose = prose.replace(
+				/\n\[\^.*?(?=\[\n\^|\n\n|$)/gs,
+				footnote => this.removeSemBr(footnote)
+			);
+
+			return prose;
 		});
 
 		// put YAML & code blocks back
-		if (yamlHeader) noteContent = yamlHeader[0] + noteContent;
 		if (noteHasCodeBlock) {
 			const tempArr = [];
 			for (let i = 0; i < proseTextArr.length; i++) {
 				tempArr.push(proseTextArr[i]);
-				tempArr.push(codeBlockArr[i]);
+				if (codeBlockArr[i])	tempArr.push(codeBlockArr[i]);
 			}
 			noteContent = tempArr.join("```");
 		} else {
 			noteContent = proseTextArr[0];
 		}
+		if (yamlHeader) noteContent = yamlHeader[0] + noteContent;
 
 		// add line break at document end back
 		noteContent += "\n";
